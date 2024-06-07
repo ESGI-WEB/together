14a2cf479635f01b82daa349b5a1a2be144d2bc0
lauriemorin <laurie20010701@gmail.com>
2024-06-07 21:17:06 +0200

    add api services decode and improve middleware on event_type router

--- front/lib/core/services/group_services.dart
+++ front/lib/core/services/group_services.dart
@@ -9,7 +9,7 @@ class GroupServices {
   static Future<List<Group>> fetchGroups(int page, int limit) async {
     try {
       final response = await ApiServices.get('/groups?page=$page&limit=$limit');
-      List<dynamic> jsonData = ApiServices.decodeRespons(response.body)['rows'];
+      List<dynamic> jsonData = ApiServices.decodeResponse(response)['rows'];
       return jsonData.map((json) => Group.fromJson(json)).toList();
     } catch (e) {
       if (e is UnauthorizedException) {
@@ -25,7 +25,7 @@ class GroupServices {
   static Future<Group> createGroup(Map<String, dynamic> newGroup) async {
     try {
       final response = await ApiServices.post('/groups', newGroup);
-      return Group.fromJson(ApiServices.decodeRespons(response));
+      return Group.fromJson(ApiServices.decodeResponse(response));
     } catch (e) {
       if (e is UnauthorizedException) {
         throw UnauthorizedException(message: "Vous n'êtes pas connecté");
@@ -40,7 +40,7 @@ class GroupServices {
   static Future<Group> joinGroup(Map<String, dynamic> code) async {
     try {
       final response = await ApiServices.post('/groups/join', code);
-      return Group.fromJson(ApiServices.decodeRespons(response.body));
+      return Group.fromJson(ApiServices.decodeResponse(response));
     } catch (e) {
       if (e is UnauthorizedException) {
         throw UnauthorizedException(message: "Vous n'êtes pas connecté");
@@ -55,7 +55,7 @@ class GroupServices {
   static Future<Group> getGroupById(int groupId) async {
     try {
       final response = await ApiServices.get('/groups/$groupId');
-      return Group.fromJson(ApiServices.decodeRespons(response));
+      return Group.fromJson(ApiServices.decodeResponse(response));
     } catch (e) {
       if (e is UnauthorizedException) {
         throw UnauthorizedException(message: "Vous n'êtes pas connecté");
@@ -71,7 +71,7 @@ class GroupServices {
     try {
       final response = await ApiServices.get('/groups/$groupId/next-event');
       if (response.body.isNotEmpty){
-        return Event.fromJson(ApiServices.decodeRespons(response));
+        return Event.fromJson(ApiServices.decodeResponse(response));
       }
       return null;
     } catch (e) {
