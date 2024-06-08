cc545983089fa3b4efce41aea19e4fb81ce8c7c9
Nicolas W <nwadoux@myges.fr>
2024-06-08 11:21:32 +0200

    fix  validators

--- back/utils/validation.go
+++ back/utils/validation.go
@@ -1,8 +1,8 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/go-playground/validator/v10"
-	"mime/multipart"
 	"reflect"
 	"strings"
 )
@@ -11,19 +11,23 @@ func GetValidationErrors(errors validator.ValidationErrors, jsonBody interface{}
 	var validationErrors = make(map[string]string)
 
 	for _, e := range errors {
-		fieldParts := strings.Split(e.StructNamespace(), ".")[1:]
+		fmt.Println(e.StructNamespace())
+		fieldParts := strings.Split(e.StructNamespace(), ".")
+		fieldPartsWithoutNamespace := fieldParts
+		if len(fieldParts) > 1 {
+			fieldPartsWithoutNamespace = fieldParts[1:]
+		}
 		currentBody := jsonBody
-		fieldNameJSON := GetJSONFieldName(currentBody, fieldParts[0])
+		fieldNameJSON := GetJSONFieldName(currentBody, fieldPartsWithoutNamespace[0])
 
-		// Traverse the jsonBody to get to the nested struct
-		for i := 0; i < len(fieldParts)-1; i++ {
-			currentBody = getField(currentBody, fieldParts[i])
+		for i := 0; i < len(fieldPartsWithoutNamespace)-1; i++ {
+			currentBody = getField(currentBody, fieldPartsWithoutNamespace[i])
 			if currentBody == nil {
 				break
 			}
 
 			if currentBody != nil {
-				fieldNameJSON += "." + GetJSONFieldName(currentBody, fieldParts[len(fieldParts)-1])
+				fieldNameJSON += "." + GetJSONFieldName(currentBody, fieldPartsWithoutNamespace[len(fieldPartsWithoutNamespace)-1])
 			}
 		}
 
@@ -51,67 +55,4 @@ func getField(jsonBody interface{}, fieldName string) interface{} {
 	}
 
 	return field.Interface()
-}
-
-// image formats and magic numbers
-var magicTable = map[string]string{
-	"\xff\xd8\xff":      "image/jpeg",
-	"\x89PNG\r\n\x1a\n": "image/png",
-	"GIF87a":            "image/gif",
-	"GIF89a":            "image/gif",
-}
-
-func MimeFromIncipit(incipit []byte) string {
-	incipitStr := string(incipit)
-	for magic, mime := range magicTable {
-		if strings.HasPrefix(incipitStr, magic) {
-			return mime
-		}
-	}
-
-	return ""
-}
-
-func IsImage(file multipart.FileHeader) bool {
-	incipit := make([]byte, 512)
-	fileSrc, err := file.Open()
-	if err != nil {
-		return false
-	}
-	defer fileSrc.Close()
-
-	_, err = fileSrc.Read(incipit)
-	if err != nil {
-		return false
-	}
-
-	return MimeFromIncipit(incipit) != ""
-}
-
-func GetImageExt(file multipart.FileHeader) string {
-	incipit := make([]byte, 512)
-	fileSrc, err := file.Open()
-	if err != nil {
-		return ""
-	}
-	defer fileSrc.Close()
-
-	_, err = fileSrc.Read(incipit)
-	if err != nil {
-		return ""
-	}
-
-	mime := MimeFromIncipit(incipit)
-	if mime != "" {
-		switch mime {
-		case "image/jpeg":
-			return ".jpg"
-		case "image/png":
-			return ".png"
-		case "image/gif":
-			return ".gif"
-		}
-	}
-
-	return ""
 }
