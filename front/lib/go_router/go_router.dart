2a262a8317f3fffaca1d6fa6300d181c943e190b
Théo Dubuisson <dubuissontheo@outlook.fr>
2024-06-09 17:11:59 +0200

    :feat: add blocs

--- front/lib/go_router/go_router.dart
+++ front/lib/go_router/go_router.dart
@@ -7,7 +7,7 @@ import 'package:front/chat/chat_screen.dart';
 import 'package:front/core/partials/custom_app_bar.dart';
 import 'package:front/core/partials/custom_bottom_bar.dart';
 import 'package:front/core/services/storage_service.dart';
-import 'package:front/event/create_event_screen.dart';
+import 'package:front/event/create_event_screen/create_event_screen.dart';
 import 'package:front/event/event_screen/event_screen.dart';
 import 'package:front/groups/create_group_screen/create_group_screen.dart';
 import 'package:front/groups/group_screen/group_screen.dart';
@@ -49,14 +49,13 @@ final goRouter = GoRouter(
           path: '/groups',
           builder: (context, state) {
             return BlocProvider(
-                create: (context) => GroupsBloc(),
-                child: const GroupsScreen());
+                create: (context) => GroupsBloc(), child: const GroupsScreen());
           },
           routes: [
             GoRoute(
               name: CreateGroupScreen.routeName,
               path: 'create',
-              builder: (context, state)  {
+              builder: (context, state) {
                 final groupsBloc = state.extra as GroupsBloc;
                 return CreateGroupScreen(groupsBloc: groupsBloc);
               },
