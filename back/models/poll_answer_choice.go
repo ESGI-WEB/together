16968901fb0b50546b00084160b5ee98d37a0604
Nicolas W <nwadoux@myges.fr>
2024-06-20 19:28:40 +0200

    add all routes for polls

--- back/models/poll_answer_choice.go
+++ back/models/poll_answer_choice.go
@@ -6,8 +6,18 @@ import (
 
 type PollAnswerChoice struct {
 	gorm.Model
-	Choice string
-	PollID uint
-	Poll   Poll
-	Users  []User `gorm:"many2many:poll_answer_choice_users"`
+	Choice string  `gorm:"not null" json:"choice" validate:"required,min=1,max=255"`
+	PollID uint    `gorm:"not null" json:"poll_id" validate:"required"`
+	Poll   *Poll   `json:"poll,omitempty"`
+	Users  *[]User `gorm:"many2many:poll_answer_choice_users" json:"users,omitempty"`
+}
+
+type PollAnswerChoiceCreateOrEdit struct {
+	Choice string `gorm:"not null" json:"choice" validate:"required,min=1,max=255"`
+}
+
+func (p *PollAnswerChoiceCreateOrEdit) ToPollAnswerChoice() PollAnswerChoice {
+	return PollAnswerChoice{
+		Choice: p.Choice,
+	}
 }
